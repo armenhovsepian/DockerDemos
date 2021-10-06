01135b59f6e040b4ed85e585eabbaff1ba35821b
MD <rmn.hovsepian@live.com>
2021-10-06 19:16:13 +0400

    Added asp.net core web api

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hello Docker!");
+            }
+        }
+    }
+}
